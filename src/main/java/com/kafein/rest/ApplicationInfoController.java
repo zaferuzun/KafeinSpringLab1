37bf5943192701464c45e217101348a91c1dcb09
Zafer UZUN <zafer.uzun@partner.turktelekom.com>
2022-03-22 16:28:15 +0300

    app-info mapping edited customer query created

--- src/main/java/com/kafein/rest/ApplicationInfoController.java
+++ src/main/java/com/kafein/rest/ApplicationInfoController.java
@@ -4,9 +4,11 @@ import com.kafein.config.AppConfiguration;
 import com.kafein.dto.ApplicationInfoDto;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
 @RestController
+@RequestMapping("/rest/v1/config")
 public class ApplicationInfoController {
 
     @Autowired
--- src/main/java/com/kafein/rest/CustomerQueryController.java
+++ src/main/java/com/kafein/rest/CustomerQueryController.java
@@ -1,4 +1,39 @@
 package com.kafein.rest;
 
+import com.kafein.config.AppConfiguration;
+import com.kafein.dto.ApplicationInfoDto;
+import com.kafein.dto.CustomerDto;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.web.bind.annotation.*;
+
+@RestController
+@RequestMapping("/rest/v1/customer")
 public class CustomerQueryController {
+
+    @Autowired
+    AppConfiguration appConfiguration;
+
+    @GetMapping("/{id}")
+    public CustomerDto getCustomerById(@PathVariable("id") final Long id){
+
+        CustomerDto customerDto = appConfiguration.getApplicationInfoDto().getDefaultCustomerDtos().stream()
+                .filter(customer -> id.equals(customer.getCustomerId()))
+                .findAny()
+                .orElse(null);
+
+        return customerDto;
+    }
+
+    @GetMapping("/name-and-surname")
+    public CustomerDto getCustomerByNameAndSurname(@RequestParam("name") final String name,
+                                                   @RequestParam("surname") final String surname){
+
+        CustomerDto customerDto = appConfiguration.getApplicationInfoDto().getDefaultCustomerDtos().stream()
+                .filter(customer -> name.equals(customer.getName()) && surname.equals(customer.getSurname()))
+                .findAny()
+                .orElse(null);
+
+        return customerDto;
+    }
+
 }
